f9d8e36a87adb176866dae7b5eb96643f3a03ace
root <1vinhhuynh@gmail.com>
2023-08-15 11:21:04 -0400

    fixing backend session id

--- back-end/server.js
+++ back-end/server.js
@@ -7,8 +7,11 @@ const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
 const os = require('os');
 const networks = os.networkInterfaces();
+const dgram = require('dgram');
+const cookieParser = require('cookie-parser'); // Add this line
 
 app.use(cors());
+app.use(cookieParser());
 
 app.use(session({
   secret: 'your-secret-key',
@@ -19,52 +22,25 @@ app.use(session({
   }),
 }))
 
-
-// This will be the unique backend session ID for all users
-const backendSessionId = uuid.v4();
-
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  store: new MemoryStore({
-    checkPeriod: 86400000 // prune expired entries every 24h
-  }),
-}));
-
 app.get('/api/data', (req, res) => {
-  // If clientSessionId doesn't exist in the session, generate a new one
-  if (!req.session.clientSessionId) {
-    req.session.clientSessionId = uuid.v4();
-  }
-
-  
-
-  const forwardedForHeader = req.headers['x-forwarded-for'];
-
-  const forwardedIpAddresses = forwardedForHeader ? forwardedForHeader.split(',') : [];
-
-  
-
-  const backendHostIpAddress = 'host.docker.internal';
-  console.log('Docker Host IP:', backendHostIpAddress);
-
   const frontendIpAddress = req.ip;
-
   const clientSessionId = req.session.clientSessionId;
 
-  const clientIpAddress = req.socket.remoteAddress;
+  // Check if backend session ID cookie already exists
+  const backendSessionId = req.cookies.backendSessionId || uuid.v4();
+  
+  // Set the backend session ID as a cookie on the client's browser if it doesn't already exist
+  if (!req.cookies.backendSessionId) {
+    res.cookie('backendSessionId', backendSessionId, { maxAge: 86400000 }); // Cookie will expire after 24 hours
+  }
 
   const backendIpAddress = req.socket.localAddress;
 
   res.json({
-    forwardedIpAddresses: forwardedIpAddresses,
     clientSessionId: clientSessionId,
-    clientIp: clientIpAddress,
     frontendIp: frontendIpAddress,
     backendIp: backendIpAddress,
     backendSessionId: backendSessionId,
-    hostendHostIpAddress: backendHostIpAddress,
   });
 });
 
--- front-end/front-end/src/App.jsx
+++ front-end/front-end/src/App.jsx
@@ -3,39 +3,34 @@ import axios from 'axios';
 import './styles.css'; // Import the CSS file
 
 const App = () => {
-  const [forwardedIpAddresses, setForwardedIpAddresses] = useState([]);
   const [clientSessionId, setClientSessionId] = useState('');
-  const [clientIp, setClientIp] = useState('');
   const [frontendIp, setFrontendIp] = useState('');
   const [backendIp, setBackendIp] = useState('');
   const [backendSessionId, setBackendSessionId] = useState('');
-  const [backendHostIp, setBackendHostIp] = useState(''); // 1. Add the new state variable
 
-  useEffect(() => {
-    const storedClientSessionId = sessionStorage.getItem('clientSessionId');
-  
-    axios.get('http://192.168.0.10:5000/api/data', {
-      data: { clientSessionId: storedClientSessionId }, // Include clientSessionId in the request payload
-      headers: {
-        'X-Forwarded-For': "192.168.0.6", // Include the frontend IP address in the X-Forwarded-For header
-      },
+
+  const storedClientSessionId = sessionStorage.getItem('clientSessionId');
+
+  useEffect( () => {
+  axios.get('http://192.168.0.8:5000/api/data', {
+    headers: {
+      'x-forwarded-for': '192.168.0.7', 
+    },
+    data: { clientSessionId: storedClientSessionId },
+  })
+    .then((response) => {
+      const data = response.data;
+
+      setFrontendIp(data.frontendIp);
+      setBackendIp(data.backendIp);
+      setBackendSessionId(data.backendSessionId);
+      setClientSessionId(storedClientSessionId || data.clientSessionId);
+
+      if (!storedClientSessionId) {
+        sessionStorage.setItem('clientSessionId', data.clientSessionId);
+      }
     })
-      .then((response) => {
-        const data = response.data;
-        setForwardedIpAddresses(data.forwardedIpAddresses);  // Set forwarded IP addresses
-        setClientIp(data.clientIp);
-        setFrontendIp(data.frontendIp);
-        setBackendIp(data.backendIp);
-        setBackendSessionId(data.backendSessionId);
-        setClientSessionId(storedClientSessionId || data.clientSessionId);
-        setBackendHostIp(data.backendHostIp); 
-
-  
-        if (!storedClientSessionId) {
-          sessionStorage.setItem('clientSessionId', data.clientSessionId);
-        }
-      })
-      .catch((error) => console.error('Error fetching data:', error));
+    .catch((error) => console.error('Error fetching data:', error));
   }, []);
   
 
@@ -44,9 +39,6 @@ const App = () => {
       <h1>Docker Swarm Info</h1>
       <div className="box">
         <h2>Client Info</h2>
-        <div>
-          <strong>Client IP Address:</strong> {clientIp}
-        </div>
         <div>
           <strong>Client Session ID:</strong> {clientSessionId}
         </div>
@@ -61,20 +53,10 @@ const App = () => {
         <h2>Backend Info</h2>
         <div>
           <strong>Backend IP Address:</strong> {backendIp}
-          <div>
-          <strong>Backend Host IP:</strong> {backendHostIp}
-        </div>
         </div>
         <div>
           <strong>Backend Session ID:</strong> {backendSessionId}
         </div>
-      </div>
-      
-      <div className="box">
-        <h2>Forwarded IP Addresses</h2>
-        {forwardedIpAddresses.map((address, index) => (
-          <div key={index}>{address}</div>
-        ))}
       </div>
     </div>
   );
